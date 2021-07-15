55d3745847ea4fb81cd98d827573de4901da6b97
tushartambi <tushartambi@gmail.com>
2021-07-15 20:31:44 +0530

    headers added to response.

--- resources/user.py
+++ resources/user.py
@@ -93,9 +93,6 @@ class UserLogin(Resource):
             })
 
             response.set_cookie('token', access_token)
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            response.headers.add('Access-Control-Allow-Headers', "*")
-            response.headers.add('Access-Control-Allow-Methods', "*")
             response.headers.add('Access-Control-Allow-Credentials', True)
 
             return response
